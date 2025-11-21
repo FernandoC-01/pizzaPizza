453c1e807acc9ae3e7503494ffa6489d69f23b22
cha-hames <chahames@gmail.com>
2025-11-21 16:59:14 -0500

    Fixing problem with background and removing some unneeded skeleton code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 import OrderList from './components/OrderList'
@@ -9,15 +7,9 @@ import OrderList from './components/OrderList'
 function App() {
 
   
-  // const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div> */}
 
       <div>
           <OrderList/>
@@ -26,7 +18,7 @@ function App() {
 
 
     
-    </>
+ 
   )
 }
 
