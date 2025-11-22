4000193c53669db1fabb9482610bc9159407e8a8
cha-hames <chahames@gmail.com>
2025-11-22 13:24:45 -0500

    Fixed router issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         {/*Route Definitions*/}
         <Routes>
           <Route path="/" element={<Home />} />  
-          <Route path="/" element={<Login />} />
+          <Route path="/Login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/cart" element={<OrderList />} />
           <Route path="/payment" element={<PaymentPage />} />
